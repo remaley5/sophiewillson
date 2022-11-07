b82be341285551c35c611cd4b3c92a93164fd03f
remaley5 <swillson@alumni.risd.edu>
2022-11-07 07:56:16 -0800

    function and basic styling dropdown, media queries for nav

--- js/init.js
+++ js/init.js
@@ -1,38 +1,18 @@
-$('form#contactForm button.submit').click(function() {
+function toggleHamburger(e) {
+  e.preventDefault();
+  var nav = $('#main-nav');
+  if(!(nav.hasClass('open'))) {
+    nav.addClass('open');
+    $(this).attr('aria-expanded', 'true');
+    //$('dialog.mobile-nav').removeAttr('open');
+  } else {
+    nav.removeClass('open');
+    $(this).attr('aria-expanded', 'false');
+    //$('dialog.mobile-nav').attr('open', 'true');
+  }
+}
 
-    $('#image-loader').fadeIn();
+jQuery(document).ready(function($) {
+  $('.mobile-btn, dialog.mobile-nav .close').on('click', toggleHamburger);
 
-    var contactName = $('#contactForm #Name').val();
-    var contactEmail = $('#contactForm #Email').val();
-    var contactSubject = $('#contactForm #Subject').val();
-    var contactMessage = $('#contactForm #Message').val();
-
-    var data = 'contactName=' + contactName + '&contactEmail=' + contactEmail +
-             '&contactSubject=' + contactSubject + '&contactMessage=' + contactMessage;
-
-    $.ajax({
-
-        type: "POST",
-        url: "inc/sendEmail.php",
-        data: data,
-        success: function(msg) {
-
-          // Message was sent
-          if (msg == 'OK') {
-             $('#image-loader').fadeOut();
-             $('#message-warning').hide();
-             $('#contactForm').fadeOut();
-             $('#message-success').fadeIn();   
-          }
-          // There was an error
-          else {
-             $('#image-loader').fadeOut();
-             $('#message-warning').html(msg);
-              $('#message-warning').fadeIn();
-          }
-
-        }
-
-    });
-    return false;
- });
+})
