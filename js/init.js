d8b91621c8ec1fc5e8c85a745aad3d4e1434b170
remaley5 <swillson@alumni.risd.edu>
2022-11-12 17:07:08 -0800

    added transitions

--- js/init.js
+++ js/init.js
@@ -2,17 +2,21 @@ function toggleHamburger(e) {
   e.preventDefault();
   var nav = $('#main-nav');
   if(!(nav.hasClass('open'))) {
-    nav.addClass('open');
-    $(this).attr('aria-expanded', 'true');
-    //$('dialog.mobile-nav').removeAttr('open');
+    $('.mobile-nav').css('display', 'block');
+    setTimeout(function() {
+      nav.addClass('open');
+      $(this).attr('aria-expanded', 'true');
+    }, 100);
+    
   } else {
     nav.removeClass('open');
     $(this).attr('aria-expanded', 'false');
-    //$('dialog.mobile-nav').attr('open', 'true');
+    setTimeout(function() {
+      $('.mobile-nav').css('display', 'none');
+    }, 1000);
   }
 }
 
 jQuery(document).ready(function($) {
   $('.mobile-btn, dialog.mobile-nav .close').on('click', toggleHamburger);
-
-})
+});
